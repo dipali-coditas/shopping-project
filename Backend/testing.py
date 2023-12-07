65b000d742a2798b950ace35e1388b72b5029fe5
Ankit <ankit.raut@coditas.com>
2023-12-07 10:29:25 +0530

    file formating

--- Backend/testing.py
+++ Backend/testing.py
@@ -1,31 +1,3 @@
-# from flask import Flask, jsonify
-# from flask_cors import CORS
-# from pymongo import MongoClient
-
-# app = Flask(__name__)
-# CORS(app)
-
-# client = MongoClient("mongodb://localhost:27017")
-
-
-# db = client["shopping"]
-# collection = db["products"]
-
-# @app.route('/get_data', methods=['GET'])
-# def get_data():
-#     # Fetch data from MongoDB
-#     cursor = collection.find({})
-#     data = list(cursor)
-
-#     # Convert ObjectId to str for JSON serialization
-#     for item in data:
-#         item['_id'] = str(item['_id'])
-#     print(data)
-#     return jsonify(data)
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug=True, port=8000)
-
 # ---------------------------with sample data----------------------------------------------
 # from flask import Flask, jsonify, request, render_template
 # from flask_cors import CORS
